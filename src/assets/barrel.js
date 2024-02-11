3ec9931336c39888f281524cae5bdaf0b3eec43b
Adrian2jr <adrian.tapia@leadonemx.com>
2024-02-10 20:11:28 -0600

    Updated naming of the export  banderas to upper case for fail deploy log

--- src/assets/barrel.js
+++ src/assets/barrel.js
@@ -3,5 +3,5 @@ export { default as avatar } from "./avatar.png";
 export { default as about } from "./about.jpg";
 
 //Banderas
-export { default as banderaMexico } from "./banderas/mexico.png";
-export { default as banderaUSA } from "./banderas/usa.png";
+export { default as banderaMexico } from "./Banderas/mexico.png";
+export { default as banderaUSA } from "./Banderas/usa.png";
